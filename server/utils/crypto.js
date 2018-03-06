c2393498c28242ae1cc8e884e6723a46d155755e
刘涛 <lt@sixsoft.cn>
2018-03-06 18:51:36 +0800

    add commit
    add commit

---
+++ server/utils/crypto.js
@@ -0,0 +1,18 @@
+var crypto = require('crypto');
+
+exports.md5 = function (content) {
+	var md5 = crypto.createHash('md5');
+	md5.update(content);
+	return md5.digest('hex');	
+}
+
+exports.toBase64 = function(content){
+	return new Buffer(content).toString('base64');
+}
+
+exports.fromBase64 = function(content){
+	if(content==null || content.lenght==0){
+		return "";
+	}
+	return new Buffer(content, 'base64').toString();
+}
